2ab1cdf77f6cdd2ee6359fb96839a1ccd2fafea2
Damien Desnoe <damien.desnoe@gmail.com>
2017-10-25 13:03:21 +0200

    Add the menu and add styles

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,24 @@
 (function () {
+  document.getElementById("nav-toggle").addEventListener("click", showHideMenu);
 
+	function showHideMenu() {
+		document.getElementById("mobile-nav").classList.toggle("active");
+		document.getElementById("nav-toggle").classList.toggle("active");
+
+		if (window.innerWidth < 1024) {
+			document.body.classList.toggle("menu-active");
+		} else {
+			if(!document.body.classList.contains("menu-active")) {
+				document.body.classList.toggle("menu-active");
+			} else {
+
+				document.getElementById("title").style.transform = "initial";
+
+				setTimeout(function () {
+					document.body.classList.toggle("menu-active");
+					document.getElementById("title").removeAttribute("style");
+				}, 500);
+			}
+		}
+	}
 })();
