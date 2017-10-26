24c33a1110e48f46ad6163a5270119e292b12e4a
Damien Desnoe <damien.desnoe@gmail.com>
2017-10-26 12:28:05 +0200

    Change ids and classes and highlight active section in the main-nav

--- js/functions.js
+++ js/functions.js
@@ -2,20 +2,20 @@
   document.getElementById("nav-toggle").addEventListener("click", showHideMenu);
 
 	function showHideMenu() {
-		document.getElementById("mobile-nav").classList.toggle("active");
+		document.getElementById("main-nav").classList.toggle("active");
 		document.getElementById("nav-toggle").classList.toggle("active");
 
 		if (window.innerWidth < 1024) {
-			document.body.classList.toggle("menu-active");
+			document.body.classList.toggle("nav-active");
 		} else {
-			if(!document.body.classList.contains("menu-active")) {
-				document.body.classList.toggle("menu-active");
+			if(!document.body.classList.contains("nav-active")) {
+				document.body.classList.toggle("nav-active");
 			} else {
 
 				document.getElementById("title").style.transform = "initial";
 
 				setTimeout(function () {
-					document.body.classList.toggle("menu-active");
+					document.body.classList.toggle("nav-active");
 					document.getElementById("title").removeAttribute("style");
 				}, 500);
 			}
